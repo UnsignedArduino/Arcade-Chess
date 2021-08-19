38e6ab1e5894ab90b5576c7c30730760c43bc67d
Ckyiu <UnsignedArduino@outlook.com>
2021-08-18 21:38:24 -0400

    Can place actual pieces as sprites

---
+++ images.g.ts
@@ -0,0 +1,150 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "black_pawn":return img`
+. . . . . . . . 
+. . . f f . . . 
+. . f f f f . . 
+. . . f f . . . 
+. . f f f f . . 
+. . . f f . . . 
+. . f f f f . . 
+. . . . . . . . 
+`;
+            case "image2":
+            case "black_rook":return img`
+. . . . . . . . 
+. f . f f . f . 
+. f . f f . f . 
+. f f f f f f . 
+. . f f f f . . 
+. . f f f f . . 
+. f f f f f f . 
+. . . . . . . . 
+`;
+            case "image3":
+            case "black_knight":return img`
+. . . . . . . . 
+. . . f f f . . 
+. f f f f f . . 
+. . f f f f f . 
+. . . f f f f . 
+. . f f f f . . 
+. f f f f f . . 
+. . . . . . . . 
+`;
+            case "image4":
+            case "black_bishop":return img`
+. . . . . . . . 
+. . . f f . . . 
+. . f f f f . . 
+. . f f . . . . 
+. . f f . f . . 
+. . f f f f . . 
+. f f f f f f . 
+. . . . . . . . 
+`;
+            case "image5":
+            case "black_king":return img`
+. . . f f . . . 
+. . . . . . . . 
+. . f . . f . . 
+. f . f f . f . 
+. . f . . f . . 
+. . . f f . . . 
+. f f f f f f . 
+. . . . . . . . 
+`;
+            case "image6":
+            case "black_queen":return img`
+. . . . . . . . 
+. . . f f . . . 
+. f . . . . f . 
+. f . f f . f . 
+. f . f f . f . 
+. . f f f f . . 
+. f f f f f f . 
+. . . . . . . . 
+`;
+            case "image7":
+            case "white_pawn":return img`
+. . . . . . . . 
+. . . 1 1 . . . 
+. . 1 1 1 1 . . 
+. . . 1 1 . . . 
+. . 1 1 1 1 . . 
+. . . 1 1 . . . 
+. . 1 1 1 1 . . 
+. . . . . . . . 
+`;
+            case "image8":
+            case "white_rook":return img`
+. . . . . . . . 
+. 1 . 1 1 . 1 . 
+. 1 . 1 1 . 1 . 
+. 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 . 
+. . 1 1 1 1 . . 
+. 1 1 1 1 1 1 . 
+. . . . . . . . 
+`;
+            case "image9":
+            case "white_knight":return img`
+. . . . . . . . 
+. . . 1 1 1 . . 
+. 1 1 1 1 1 . . 
+. . 1 1 1 1 1 . 
+. . . 1 1 1 1 . 
+. . 1 1 1 1 . . 
+. 1 1 1 1 1 . . 
+. . . . . . . . 
+`;
+            case "image10":
+            case "white_bishop":return img`
+. . . . . . . . 
+. . . 1 1 . . . 
+. . 1 1 1 1 . . 
+. . 1 1 . . . . 
+. . 1 1 . 1 . . 
+. . 1 1 1 1 . . 
+. 1 1 1 1 1 1 . 
+. . . . . . . . 
+`;
+            case "image11":
+            case "white_king":return img`
+. . . 1 1 . . . 
+. . . . . . . . 
+. . 1 . . 1 . . 
+. 1 . 1 1 . 1 . 
+. . 1 . . 1 . . 
+. . . 1 1 . . . 
+. 1 1 1 1 1 1 . 
+. . . . . . . . 
+`;
+            case "image12":
+            case "white_queen":return img`
+. . . . . . . . 
+. . . 1 1 . . . 
+. 1 . . . . 1 . 
+. 1 . 1 1 . 1 . 
+. 1 . 1 1 . 1 . 
+. . 1 1 1 1 . . 
+. 1 1 1 1 1 1 . 
+. . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Piece = SpriteKind.create()
+}
 function set_variables () {
     controls_enabled = true
 }
@@ -7,6 +10,58 @@ function set_tilemap (with_tile_pieces: boolean) {
         tiles.setSmallTilemap(tilemap`board_with_tile_pieces`)
     } else {
         tiles.setSmallTilemap(tilemap`board`)
+    }
+}
+function place_pieces () {
+    tiles.destroySpritesOfKind(SpriteKind.Piece)
+    chess_tiles = [
+    assets.tile`white_pawn`,
+    assets.tile`white_rook`,
+    assets.tile`white_knight`,
+    assets.tile`white_bishop`,
+    assets.tile`white_king`,
+    assets.tile`white_queen`,
+    assets.tile`black_pawn`,
+    assets.tile`black_rook`,
+    assets.tile`black_knight`,
+    assets.tile`black_bishop`,
+    assets.tile`black_king`,
+    assets.tile`black_queen`
+    ]
+    chess_images = [
+    assets.image`white_pawn`,
+    assets.image`white_rook`,
+    assets.image`white_knight`,
+    assets.image`white_bishop`,
+    assets.image`white_king`,
+    assets.image`white_queen`,
+    assets.image`black_pawn`,
+    assets.image`black_rook`,
+    assets.image`black_knight`,
+    assets.image`black_bishop`,
+    assets.image`black_king`,
+    assets.image`black_queen`
+    ]
+    chess_names = [
+    "white pawn",
+    "white rook",
+    "white knight",
+    "white bishop",
+    "white king",
+    "white queen",
+    "black pawn",
+    "black rook",
+    "black knight",
+    "black bishop",
+    "black king",
+    "black queen"
+    ]
+    for (let index = 0; index <= chess_tiles.length - 1; index++) {
+        for (let location of tiles.getTilesByType(chess_tiles[index])) {
+            sprite_piece = sprites.create(chess_images[index], SpriteKind.Piece)
+            tiles.placeOnTile(sprite_piece, location)
+            sprites.setDataString(sprite_piece, "type", chess_names[index])
+        }
     }
 }
 function make_cursor () {
@@ -43,11 +98,17 @@ function enable_controls (enable: boolean) {
 }
 let sprite_cursor_pointer: Sprite = null
 let sprite_cursor: Sprite = null
+let sprite_piece: Sprite = null
+let chess_names: string[] = []
+let chess_images: Image[] = []
+let chess_tiles: Image[] = []
 let controls_enabled = false
 set_variables()
 make_cursor()
 enable_controls(true)
 set_tilemap(true)
+place_pieces()
+set_tilemap(false)
 game.onUpdate(function () {
     sprite_cursor.top = sprite_cursor_pointer.top
     sprite_cursor.left = sprite_cursor_pointer.left
