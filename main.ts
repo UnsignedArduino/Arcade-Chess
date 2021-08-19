ada65d7ac81e712aedf77a743cb2c95c4e722ab3
Ckyiu <UnsignedArduino@outlook.com>
2021-08-18 22:40:05 -0400

    Can select pieces

--- main.ts
+++ main.ts
@@ -3,6 +3,16 @@ namespace SpriteKind {
 }
 function set_variables () {
     controls_enabled = true
+    sprite_selected = null
+    in_game = false
+}
+function get_overlapping_sprite (target: Sprite, kind: number) {
+    for (let sprite of sprites.allOfKind(kind)) {
+        if (target.overlapsWith(sprite)) {
+            return sprite
+        }
+    }
+    return [][0]
 }
 function set_tilemap (with_tile_pieces: boolean) {
     scene.setBackgroundColor(13)
@@ -12,6 +22,22 @@ function set_tilemap (with_tile_pieces: boolean) {
         tiles.setSmallTilemap(tilemap`board`)
     }
 }
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (in_game) {
+        unselect_piece()
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (in_game) {
+        if (is_sprite(sprite_selected)) {
+        	
+        } else {
+            if (is_sprite(get_overlapping_sprite(sprite_cursor_pointer, SpriteKind.Piece))) {
+                select_piece(get_overlapping_sprite(sprite_cursor_pointer, SpriteKind.Piece))
+            }
+        }
+    }
+})
 function place_pieces () {
     tiles.destroySpritesOfKind(SpriteKind.Piece)
     chess_tiles = [
@@ -92,6 +118,9 @@ function make_cursor () {
     sprite_cursor.z = 10
     sprite_cursor_pointer.z = 10
 }
+function select_piece (sprite: Sprite) {
+    sprite_selected = sprite
+}
 function enable_controls (enable: boolean) {
     controls_enabled = enable
     if (enable) {
@@ -100,19 +129,31 @@ function enable_controls (enable: boolean) {
         controller.moveSprite(sprite_cursor_pointer, 0, 0)
     }
 }
-let sprite_cursor_pointer: Sprite = null
+function unselect_piece () {
+    sprite_selected = null
+    set_tilemap(false)
+}
+function is_sprite (sprite: Sprite) {
+    sprite = sprite
+    return sprite && !(spriteutils.isDestroyed(sprite))
+}
+let sprite: Sprite = null
 let sprite_cursor: Sprite = null
 let sprite_piece: Sprite = null
 let chess_names: string[] = []
 let chess_images: Image[] = []
 let chess_tiles: Image[] = []
+let sprite_cursor_pointer: Sprite = null
+let sprite_selected: Sprite = null
 let controls_enabled = false
+let in_game = false
 set_variables()
 make_cursor()
 enable_controls(true)
 set_tilemap(true)
 place_pieces()
 set_tilemap(false)
+in_game = true
 game.onUpdate(function () {
     sprite_cursor.top = sprite_cursor_pointer.top
     sprite_cursor.left = sprite_cursor_pointer.left
